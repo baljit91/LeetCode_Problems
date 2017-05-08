6de2cbb6fed8e52731ae74879877427f73502c25
baljit91 <baljitdhanjal@gmail.com>
2017-05-08 11:03:57 -0400

    Update MinPathSum.java

--- MinPathSum.java
+++ MinPathSum.java
@@ -1,12 +1,14 @@
 public class MinPathSum {
     static int[][] K;
+    
     public int minPathSum(int[][] grid) {
         K = new int[grid.length + 1][grid[0].length + 1];
-        return minPathSumHelper(grid,0,0);
-         
+        return minPathSumHelper(grid,0,0);   
     }
     
+    
     public int minPathSumHelper(int[][] grid, int row, int col) {
+        //going outside the boundary
         if(row == grid.length || col == grid[0].length){
             if(K[row][col] == 0){
                K[row][col] = Integer.MAX_VALUE; 
@@ -14,18 +16,21 @@ public class MinPathSum {
             return K[row][col];
         }
 
-
+        //reaching the final destination
         if(row == grid.length - 1 && col == grid[0].length - 1)
             return grid[row][col];
         
+        //exploring the right cell
         if(K[row + 1][col] == 0){
             K[row + 1][col] = minPathSumHelper(grid, row + 1, col);
         }
         
+        //exploring the down cell
         if(K[row][col + 1] == 0){
             K[row][col + 1] = minPathSumHelper(grid, row,col + 1);
         }
-            
+        
+        //returning the the min path
         return  grid[row][col] + Math.min(K[row + 1][col],K[row][col + 1]);
     }
 }
