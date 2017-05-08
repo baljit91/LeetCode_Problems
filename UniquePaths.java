0e9af9f5a4585d9ee3162907aa884b0d9b1f3c50
baljit91 <baljitdhanjal@gmail.com>
2017-05-08 11:00:48 -0400

    Update UniquePaths.java

--- UniquePaths.java
+++ UniquePaths.java
@@ -8,25 +8,30 @@ public class UniquePaths {
     }
     
     int uniquePathsHelper(int grid[][], int row, int col){
+        
+        // going outside the boundary
         if(row == grid.length || col == grid[0].length){
             return 0;
         }
         
+        //reaching the final destination
         if(row == grid.length-1 && col == grid[0].length -1){
             return 1;
         }
         
         int ways = 0;
         
-        
+        //explore to the right of the cell
         if(K[row + 1][col] == 0){
             K[row + 1][col] = uniquePathsHelper(grid, row + 1, col);
         }
         
+        //explore to the down of the cell
         if(K[row][col + 1] == 0){
             K[row][col + 1] = uniquePathsHelper(grid, row, col + 1);
         }
         
+        // adding the no. of ways returned by the down cell and right cell
         ways =  K[row + 1][col] + K[row][col + 1];
         return ways;
     }
